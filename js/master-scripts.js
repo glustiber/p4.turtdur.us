18a7c1fa8cb34e304b17956a83f0834034a5f1e8
Gregory Lustiber <glustiber@gmail.com>
2013-12-09 23:20:43 -0600

    Got RT API sort of working

--- js/master-scripts.js
+++ js/master-scripts.js
@@ -1,12 +1,4 @@
-$(document).ready(function() {
-   $(window).resize();
-});
 
-
-$(window).resize(function() {
-   if ($(this).width() > 1024) {
-
-$(document).ready(function () {
   $('#nav-list > li > a').click(function(){
     if ($(this).attr('class') != 'active'){
       $('#nav-list li ul').slideUp();
@@ -15,54 +7,28 @@ $(document).ready(function () {
       $(this).addClass('active');
     }
   });
-});
 
-   }
+$('#search-bar-submit').click(function() {
 
-   else {
-
-   	$(document).ready(function () {
-  $('#nav-list > li > a').click(function(){
-    if ($(this).attr('class') != 'active'){
-      $('#nav-list li a').removeClass('active');
-      $(this).addClass('active');
-    }
-  });
-});
-
-   }
-
-
-
-});
-
-
-$(document).ready(function() {
 
 var apikey = "u8fuatjbufuhjzsq8rtg9bgt";
 var baseUrl = "http://api.rottentomatoes.com/api/public/v1.0";
 
 // construct the uri with our apikey
 var moviesSearchUrl = baseUrl + '/movies.json?apikey=' + apikey;
-var query = $('#search-bar-input').val;
-
-
-//console.log(query);
-
-$('#search-bar-submit').click(function() {
+var query = $('#search-bar-input').val();
 
 
   // send off the query
   $.ajax({
-    url: moviesSearchUrl + '&q=' + encodeURI(query),
+    url: moviesSearchUrl + '&q=' + encodeURI(query) + '&page_limit=25',
     dataType: "jsonp",
     success: searchCallback
   });
-});
 
 // callback for when we get back the results
 function searchCallback(data) {
- /*$('.inner-content').append('Found ' + data.total + ' results for ' + query);*/
+ $('.inner-content').append('Found ' + data.total + ' results for ' + query);
  var movies = data.movies;
  $.each(movies, function(index, movie) {
    $('.inner-content').append('<h2>' + movie.title + '</h2>');
@@ -71,3 +37,7 @@ function searchCallback(data) {
 }
 
 });
+
+
+
+
